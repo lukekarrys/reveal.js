284c7540026a55bd938bbf328108d50a838b5ec2
Luke Karrys <lukekarrys@gmail.com>
2015-02-25 13:23:00 -0700

    update export script

--- export.sh
+++ export.sh
@@ -5,10 +5,10 @@ DIR=$1
 rsync -avzrR \
     css/print/* \
     css/theme/andyet.css \
-    css/reveal.min.css \
+    css/reveal.css \
     images/logos/* \
     images/clouds.png \
-    js/reveal.min.js \
+    js/reveal.js \
     lib/* \
     plugin/* \
     index.html \
